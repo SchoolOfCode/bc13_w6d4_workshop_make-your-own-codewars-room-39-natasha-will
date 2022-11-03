c37da900dbe5f36c61c1cb4422d1a48b371764c9
willbyrne <will.bl.byrne@gmail.com>
2022-11-03 14:09:12 +0000

    started plan

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 /* 
-👉 Write your kata here!
-*/
+Themes:
+- array - pushing elements in an array
+- array - finding something within nested arrays
+- counting the letters in a string - maybe with random symbols
+- sum of two array elements - like kata from yesterday
+- zombie border control - splitting into two arrays using pattern recognition
 
+
+
+
+[
+{personfile: {name:, city:, blood-type:}},
+{personfile: [{name:}, {city:}, {blood-type:}]},
+{personfile: [{name:}, {city:}, {blood-type:}]},
+{personfile: [{name:}, {city:}, {blood-type:}],
+]
+
+test for 
+
+👉 Write your kata here!
+ Scenario:
+Oh, no a zombie virus has broken out but fortunately you live in the safehaven of Codeville. 
+
+You are the border guard checking that the people entering are not infected with the zombie virus. You have a list of names of people and their details that want to enter Codeville. Look for any suspicious details that may suggest they are secretly infected!
+
+*/
 //👉 Write the function your CodeWarriors will start with below here:
+
+function getHuman(entryList){
+
+}
