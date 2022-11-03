bf68c7e32afebd6153826344cefd1e820f1cdde0
willbyrne <will.bl.byrne@gmail.com>
2022-11-03 16:31:19 +0000

    version before release

--- main.js
+++ main.js
@@ -1,39 +1,23 @@
-/* 
-Themes:
-- array - pushing elements in an array
-- array - finding something within nested arrays
-- counting the letters in a string - maybe with random symbols
-- sum of two array elements - like kata from yesterday
-- zombie border control - splitting into two arrays using pattern recognition
-
-
-
-
-[
-{personfile: {name:, city:, blood-type:}},
-{personfile: [{name:}, {city:}, {blood-type:}]},
-{personfile: [{name:}, {city:}, {blood-type:}]},
-{personfile: [{name:}, {city:}, {blood-type:}],
-]
-
-test for 
+/*  
 
 👉 Write your kata here!
  Scenario:
-Oh, no a zombie virus has broken out but fortunately you live in the safehaven of Codeville. 
+Oh, no a zombie virus has broken out but fortunately you live in the safehaven of Codeville.😪 
 
 You are the border guard checking that the people entering are not infected with the zombie virus. You have a list of names of people and their details that want to enter Codeville. Look for any suspicious details that may suggest they are secretly infected!
+
+Watch out, Zombies are really bad at spelling their names. They also tend to come from the capital of Zombieland & have a special letter in their blood type.
+
+Example entryList:
+[
+        {personfile: {name: "Dani$l", city: "Zombieville", bloodType:"OZ-negative"}},
+        {personfile: {name:"Andrea", city:"Austin", bloodType:"O-positive"}},
+]
 
 */
 //👉 Write the function your CodeWarriors will start with below here:
 
 export function getHumans(entryList){
-    // loop through array within object
-    //name - just letters in the string
-    // city - !== Zombieville
-    // blood type should not have a Z in string
-    // add personfile which has these requirements to array
-//     // return array
     let humans = [];
     for (let i = 0; i < entryList.length; i++){
         let name = entryList[i].personfile.name 
@@ -44,15 +28,14 @@ export function getHumans(entryList){
         let zPattern = /[zZ]+/g;
         let bloodResult = zPattern.test(bloodType);
             if ( result == false){
-            
+                if ( city !== 'Zombieville') { 
+                    if ( bloodResult == false) {
+                    humans.push(entryList[i]) 
+                    }
+                }
             }
-            if ( city !== 'Zombieville') {
-               
-            }
-            if ( bloodResult == false) {
-                humans.push(entryList[i]) 
-            }
-            
+
+
         }
         return humans
  }
--- main.test.js
+++ main.test.js
@@ -1,18 +1,14 @@
 import { getHumans } from './main.js'
 
 //👉 Write your tests below here:
-//[
-// {personfile: {name: "Dani$l", city: "Zombieville", blood-type:"OZ-negative"}},
-// {personfile: [{name:"Andrea"}, {city:"Austin"}, {blood-type:"O-positive"}]},
-// {personfile: [{name:"Pablo"}, {city:"MexicoDF"}, {blood-type:"AB-positive"}]},
-// {personfile: [{name:"V1ct@r"}, {city:"Zurich"}, {blood-type:"OZ-positive"}],
-// ]
 
 test('getHumans function takes in an array and return a personfile', () => {
     const entryList = [
         {personfile: {name: "Dani$l", city: "Zombieville", bloodType:"OZ-negative"}},
         {personfile: {name:"Andrea", city:"Austin", bloodType:"O-positive"}},
         {personfile: {name:"Pablo", city:"Mexico City", bloodType:"AB-positive"}},
+        {personfile: {name:"Gary", city:"Zombieville", bloodType:"AB-positive"}},
+        {personfile: {name:"Peter", city:"London", bloodType:"AZ-positive"}},
         {personfile: {name:"V1ct@r", city:"Zurich", bloodType:"OZ-positive"}},
         ]
     const actual = getHumans(entryList);
@@ -20,6 +16,32 @@ test('getHumans function takes in an array and return a personfile', () => {
         {personfile: {name:"Andrea", city:"Austin", bloodType:"O-positive"}},
         {personfile: {name:"Pablo", city:"Mexico City", bloodType:"AB-positive"}},
     ]
+    expect(actual).toStrictEqual(expected);
+   
+})
+
+
+test('getHumans function takes in an empty array & return an empty array', () => {
+    const entryList = []
+    const actual = getHumans(entryList);
+    const expected = []
+    expect(actual).toStrictEqual(expected);
+   
+})
+
+
+test('getHumans function takes in an array full of zombies & returns empty array', () => {
+    const entryList = [
+        {personfile: {name: "L0z", city: "Birmingham", bloodType:"OZ-negative"}},
+        {personfile: {name: "Dani$l", city: "Zombieville", bloodType:"OZ-negative"}},
+        {personfile: {name:"Andre@", city:"Austin", bloodType:"O-positive"}},
+        {personfile: {name:"Pablo", city:"Mexico City", bloodType:"ABZ-positive"}},
+        {personfile: {name:"Gary", city:"Zombieville", bloodType:"AB-positive"}},
+        {personfile: {name:"Peter", city:"London", bloodType:"AZ-positive"}},
+        {personfile: {name:"V1ct@r", city:"Zurich", bloodType:"OZ-positive"}},
+        ]
+    const actual = getHumans(entryList);
+    const expected = []
     expect(actual).toStrictEqual(expected);
    
 })
